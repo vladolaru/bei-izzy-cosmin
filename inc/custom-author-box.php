2173ba949252875b95a68c34f39c5d6725c1c3d1
BurloiuCosmin <cosmin.burloiu@yahoo.com>
2018-09-27 17:19:43 +0300

    finished with all the leftovers, remains to make theme options, author box, to sanitize and pig latin

---
+++ inc/custom-author-box.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cosmin
+ * Date: 9/27/2018
+ * Time: 2:07 PM
+ */
---
+++ inc/custom-theme-options.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cosmin
+ * Date: 9/27/2018
+ * Time: 3:33 PM
+ */
